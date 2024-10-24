b5934117b093bb2dd80312138fa591ae0401840d
denizsoyhan <denizsoyhan@gasai.kali>
2024-10-24 19:14:24 +0300

    second part of the day3

--- 2016/day3/day3.js
+++ 2016/day3/day3.js
@@ -23,7 +23,7 @@ for (var i=0; i<dataLine.length; i++){
     dataLine[i]=dataLine[i].trim();
     var nums=dataLine[i].split("  ");
 
-
+  
     if (nums[0]!=''){
         var a = Number(nums[0]);
         var b = Number(nums[1]);
@@ -55,14 +55,68 @@ function questionTwo(){
     
     var resCount=0;
     var threeTriangles=[];
-
+    var time=0;
+    var lineCounter=0;
     for (var i=0; i<dataLine.length; i++){
 
         dataLine[i]=dataLine[i].trim();
         var nums=dataLine[i].split("  ");
+        nums = nums.filter(num => num !== '');
+      
+        //console.log(nums);
+ 
+        if (nums[0]!=''){
+            lineCounter++;
+            if(time%3==0){
+                var a = Number(nums[0]);
+                var one = Number(nums[1]);
+                var first = Number(nums[2]);
 
-    }
+            }else if(time%3==1){
+                var b = Number(nums[0]);
+                var two = Number(nums[1]);
+                var second = Number(nums[2]);
 
+            }else if(time%3==2){
+                var c = Number(nums[0]);
+                var three = Number(nums[1]);
+                var third = Number(nums[2]);
+
+                if(
+                    ((a+b)>c) && 
+                    ((a+c)>b) && 
+                    ((b+c)>a)
+                ){
+
+                    resCount++;
+                }
+                if(
+                    ((one+two)>three) && 
+                    ((one+three)>two) && 
+                    ((two+three)>one)
+                ){  
+                    resCount++;
+                }
+                if(
+                    ((first+second)>third) && 
+                    ((first+third)>second) && 
+                    ((second+third)>first)
+                ){
+
+                    resCount++;
+                }
+            }
+
+            time++;
+        }else{   
+
+                continue;
+            }
+    
+}   
+
+    console.log(`result: ${resCount}`);
 }
-questionOne();
+//questionOne();
+
 //questionTwo();
