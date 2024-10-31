a1a99e3ef743f6ef7d3544c2058664c8285e16c7
denizsoyhan <denizsoyhan@gasai.kali>
2024-10-31 18:18:03 +0300

    second part of day5

--- 2016/day5/day5.js
+++ 2016/day5/day5.js
@@ -26,4 +26,33 @@ console.log(digits.join(""));
 
 }
 
-questionOne();
+function questionTwo(){
+var testStringStart="abc";
+var questionOneInput="reyedfim";
+
+var counter=0;
+var index=0;
+
+var hash;
+var digits=[]
+var digitFlag=[0, 0, 0, 0, 0, 0, 0, 0];
+var digitInOrder=[0, 0, 0, 0, 0, 0, 0, 0];
+while(counter<8){
+
+    hash = crypto.createHash('md5').update(questionOneInput).update(String(index)).digest('hex');
+    var firstFiveChar=hash.slice(0,5);
+    if(firstFiveChar=='00000' && hash[5]<8 && digitFlag[hash[5]]!=1){
+        //console.log(hash);
+        digitInOrder[hash[5]]=hash[6];
+        digitFlag[hash[5]]=1;
+        counter++;
+       console.log(digitInOrder.join(""));
+    }
+    index++;
+}
+
+console.log("behold: "+ digitInOrder.join(""));
+
+}
+//questionOne();
+//questionTwo();
