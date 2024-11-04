e0eb9f2b1b9538725f4b29ef991a19ff552daa85
denizsoyhan <oguldenizsoyhan@gmail.com>
2024-11-04 15:32:21 +0300

    2nd part of day6

--- 2016/day6/day6.js
+++ 2016/day6/day6.js
@@ -55,4 +55,62 @@ function questionOne(){
 
 }
 
-questionOne();
+function questionTwo(){
+    var data=textFile.toString();
+    data= data.split("\n");
+
+    let lenOfPhrase;
+    lenOfPhrase=data[0].length;
+
+    var allDigits=[]
+
+    var dictionary={
+
+    };
+
+    for(var i=0;i<lenOfPhrase;i++){
+        for(var z=0;z<26;z++){
+            dictionary[`${String.fromCharCode(97+z)}`]=0;
+        }
+        
+
+        for(var k=0;k<data.length;k++){
+            if(data[k] != '\n' ){
+              
+                dictionary[`${data[k][i]}`]=dictionary[data[k][i]]+1;
+                
+            }
+            
+        }
+        
+        
+        var arrayOfItems=Object.keys(dictionary).map(function(key){
+            return [key,dictionary[key]];
+        })
+        arrayOfItems.sort(function(a,b){
+            return a[1]-b[1];
+        })
+  
+        let aDigi;
+        for(let index=0;index<arrayOfItems.length-1;index++){
+            if(arrayOfItems[index][1]!=0){
+  
+                aDigi=arrayOfItems[index][0];
+                break;
+            }else{
+                continue;
+            }
+        }
+        
+        allDigits.push(aDigi);
+        
+       
+    }
+    
+
+    console.log(allDigits.join(""));
+
+}
+
+//questionOne();
+//questionTwo();
